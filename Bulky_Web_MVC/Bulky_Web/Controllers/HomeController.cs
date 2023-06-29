dec8d3cc35c1c84f9633c8d4d6c9ed44625c9fd6
sisaha21976 <sinchan.saha@hyland.com>
2023-06-29 18:01:18 +0530

    commiting first model / dto

--- Bulky_Web_MVC/Bulky_Web/Controllers/HomeController.cs
+++ Bulky_Web_MVC/Bulky_Web/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Bulky_Web.Controllers
         {
             _logger = logger;
         }
-
+        
         public IActionResult Index()
         {
             return View();
---
+++ Bulky_Web_MVC/Bulky_Web/Models/Category.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bulky_Web.Models
+{
+    public class Category
+    {
+
+        //key annotation similar to @Id
+        [Key]
+        public int Id { get; set; }
+        
+        //required is non-nullable data field
+        [Required]
+        public string Name { get; set; }
+        public int Displayorder { get; set; }
+
+        public Category()
+        {
+
+        }
+        
+
+        public Category(int id, string name, int displayorder)
+        {
+            Id = id;
+            Name = name;
+            Displayorder = displayorder;
+        }
+    }
+}
