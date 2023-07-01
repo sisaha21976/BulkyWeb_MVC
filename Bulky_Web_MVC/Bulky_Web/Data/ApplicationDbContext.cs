10f54ed8b4322d6c39e873d793996eb96e2ac8b0
sisaha21976 <sinchan.saha@hyland.com>
2023-07-01 10:15:18 +0530

    Creating database connection and creating database in sql server via code

---
+++ Bulky_Web_MVC/Bulky_Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bulky_Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        /*
+         here we will pass the DbcontextOption while creating the object of applicationDbContext, and : base(option) will
+         pass the same parameters to the parent as well  .i.e. DbContext
+         */
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Bulky_Web_MVC/Bulky_Web/Program.cs
+++ Bulky_Web_MVC/Bulky_Web/Program.cs
@@ -1,7 +1,20 @@
+using Bulky_Web.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//register everything here, this is similar to spring boot start class, where we can have all the configurations done
+//adding the DB Context
+/*
+ * pass the which option we wanna select (UseSqlServer will signify that we wann use sql server
+ * inside that we are basically retrieving the connectionString named DefaultConnection from connectionStrings
+ */
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
